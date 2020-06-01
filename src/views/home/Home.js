0778aecc79cf7b76809e2bfa367b133d0792e806
kol <18703535626@163.com>
2020-06-01 19:10:27 +0800

    动画

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,25 +1,65 @@
 export default {
-    components: {},
-    data() {
-        return {
-            scrollTimeout:null,
-        };
+  components: {},
+  data() {
+    return {
+      scrollTimeout: null,
+      showLogin:true,
+      showUser:false,
+      nowPage:0 //当前幻灯片的页码
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    scrollCtrl(isNext) {
+      clearTimeout(this.scrollTimeout);
+      this.scrollTimeout = setTimeout(() => {
+        if (isNext) {
+          this.$refs.carousel.next();
+        } else {
+          this.$refs.carousel.prev();
+        }
+      }, 200);
     },
-    computed: {},
-    watch: {},
-    created() {},
-    mounted() {},
-    methods: {
-        scrollCtrl(isNext){
-            clearTimeout(this.scrollTimeout)
-            this.scrollTimeout=setTimeout(()=>{
-                if(isNext){
-                    this.$refs.carousel.next()
-                }else{
-                    this.$refs.carousel.prev()
-                }
-            },200)
-        },
-       
+    changeCarousel(nowPage,oldPage){
+        this.nowPage=nowPage
     },
-}
+    toLogin() {
+      this.$refs.loginCircle.classList.add("active");
+      setTimeout(() => {
+        this.$refs.loginCircle.classList.remove("active");
+        this.$refs.left.style.transform='translateX(-8000px)'
+        this.$refs.right.style.transform='translateX(8000px)'
+        setTimeout(()=>{
+            this.showLogin=false
+            let width = this.$refs.block.offsetWidth
+            let height = this.$refs.block.offsetHeight
+            console.log(width,height)
+            //改变三角形宽高
+            this.$nextTick(()=>{
+                this.$refs.topTriangle[0].style.borderTop=height+'px solid transparent'
+                this.$refs.topTriangle[0].style.borderLeft=width+'px solid #84dc8b59'
+                setTimeout(()=>{
+                    this.$refs.topTriangle[0].style.transform='translateX(0)'
+                },0)
+                setTimeout(()=>{
+                    this.showUser=true
+                },2000)
+
+                this.$refs.bottomTriangle[0].style.borderBottom=height+'px solid transparent'
+                this.$refs.bottomTriangle[0].style.borderRight=width+'px solid #e871be7d'
+                setTimeout(()=>{
+                    this.$refs.bottomTriangle[0].style.transform='translateX(0)'
+                },0)
+
+
+            })
+
+        },300)
+
+      }, 500);
+    },
+  },
+};
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,59 @@
 <template>
   <div class="container" v-scroll="scrollCtrl">
-    <div class="loginMask">
-        <div class="loginCircle">登录</div>
-        <div class="left"></div>
-        <div class="right"></div>
+    <div class="loginMask" v-if="showLogin">
+      <div class="loginCircle" ref="loginCircle" @click="toLogin">登录</div>
+      <div class="left" ref="left"></div>
+      <div class="right" ref="right"></div>
     </div>
-    <div class="block">
-      <el-carousel height="100%" direction="vertical" :autoplay="false" ref="carousel">
-        <el-carousel-item class="homePic" v-for="item in 3" :key="item"></el-carousel-item>
+    <div class="block" ref="block">
+      <el-carousel
+        height="100%"
+        direction="vertical"
+        @change="changeCarousel"
+        :autoplay="false"
+        ref="carousel"
+      >
+        <el-carousel-item class="homePic" v-for="item in 3" :key="item">
+          <div class="triangleWrap" v-show="nowPage==0&&!showLogin">
+            <div class="topTriangle" ref="topTriangle"></div>
+            <div class="bottomTriangle" ref="bottomTriangle"></div>
+            <div class="contentWrap" v-if="showUser">
+              <div class="content">
+                <div class="user">
+                  <span class="photo"></span>
+                  <span class="value">Mr Wen</span>
+                </div>
+                <div class="weight">
+                  <span class="name">体重:</span>
+                  <span class="value">80</span>kg
+                </div>
+                <!-- 日消耗总热量 -->
+                <div class="consume">
+                  <span class="name">日消耗总热量:</span>
+                  <span class="value">2558.875</span>
+                </div>
+              </div>
+              <!-- ----------- -->
+              <div class="content2">
+                <div class="user">
+                  <span class="photo">
+                      <img src="../../assets/images/bds.png" alt="">
+                  </span>
+                  <span class="value">Miss Wang</span>
+                </div>
+                <div class="weight">
+                  <span class="name">体重:</span>
+                  <span class="value">80</span>kg
+                </div>
+                <!-- 日消耗总热量 -->
+                <div class="consume">
+                  <span class="name">日消耗总热量:</span>
+                  <span class="value">2558.875</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </el-carousel-item>
       </el-carousel>
     </div>
   </div>
